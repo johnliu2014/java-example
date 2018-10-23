32d6f384760d0e67b881d6de9f59e042b8346782
liuxiaodong@zknext.com <liuxiaodong@zknext.com>
2018-10-23 09:58:19 +0800

    initial commit

---
+++ Comment.java
@@ -0,0 +1,8 @@
+class Comment {
+    // This is a end-of-line comment; it extends to the end of the line.
+
+    /* This is a delimited comment,
+    extending over several line
+    */
+    int /* This delimited comment extends over part of a line */ x = 117;
+}
